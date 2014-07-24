8ac297233763b73232210633323b7a1d599f5518
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-07-24 19:27:26 -0400

    Guard against missing basic auth input

--- harvest/api_client.go
+++ harvest/api_client.go
@@ -1,19 +1,26 @@
+/*
+Package harvest provides data structures and a wrapper for the Harvest API
+*/
 package harvest
 
 import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 
 	"code.google.com/p/goauth2/oauth"
 )
 
 type APIClient struct {
+	// Authentication and connection information
 	username   string
 	password   string
 	subdomain  string
 	httpClient *http.Client
 
+	// Data interface accessors
 	Client          *ClientService
 	People          *PersonService
 	Project         *ProjectService
@@ -21,7 +28,7 @@ type APIClient struct {
 	Account         *AccountService
 	Task            *TaskService
 	Contact         *ContactService
-	ExpenseCategory *ExpenseCategoryService
+	ExpenseCategory *ExpenseService
 	Entry           *EntryService
 }
 
@@ -42,18 +49,38 @@ func newAPIClient(subdomain string, httpClient *http.Client) (c *APIClient) {
 	c.Account = &AccountService{Service{c}}
 	c.Task = &TaskService{Service{c}}
 	c.Contact = &ContactService{Service{c}}
-	c.ExpenseCategory = &ExpenseCategoryService{Service{c}}
+	c.ExpenseCategory = &ExpenseService{Service{c}}
 	c.Entry = &EntryService{Service{c}}
 	return c
 }
 
+// NewAPIClientWithBasicAuth instantiates a new http.Client and returns a new
+// APIClient using HTTP basic auth credentials
 func NewAPIClientWithBasicAuth(username, password, subdomain string) (c *APIClient) {
+	var missingData []string
+	if subdomain == "" {
+		missingData = append(missingData, "subdomain")
+	}
+	if username == "" {
+		missingData = append(missingData, "username")
+	}
+	if password == "" {
+		missingData = append(missingData, "password")
+	}
+	errorMsg := strings.Join(missingData, ", ")
+	if errorMsg != "" {
+		fmt.Println("ERROR! You are missing the following:", errorMsg)
+		os.Exit(1)
+	}
+
 	c = newAPIClient(subdomain, nil)
 	c.username = username
 	c.password = password
 	return c
 }
 
+// NewAPIClientWithAuthToken instantiates a new http.Client and returns a new
+// APIClient using an OAuth token
 func NewAPIClientWithAuthToken(token, subdomain string) (c *APIClient) {
 	t := &oauth.Transport{
 		Token: &oauth.Token{AccessToken: token},
@@ -62,6 +89,8 @@ func NewAPIClientWithAuthToken(token, subdomain string) (c *APIClient) {
 	return c
 }
 
+// GetJSON makes an HTTP GET request to the specified path and returns the body
+// of the HTTP response
 func (c *APIClient) GetJSON(path string) (jsonResponse []byte, err error) {
 	resourceURL := fmt.Sprintf("https://%v.harvestapp.com%v", c.subdomain, path)
 	request, err := http.NewRequest("GET", resourceURL, nil)
